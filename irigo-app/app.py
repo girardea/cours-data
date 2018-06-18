cd5b59671365e7d968db9d84a25a04c671cec883
Pierre Girardeau <pierre.girardeau@gmail.com>
2018-06-18 14:50:50 +0200

    Started process in app.py.

--- irigo-app/app.py
+++ irigo-app/app.py
@@ -4,3 +4,16 @@
 """
     Lancement du serveur qui fait tourner l'app complète (le tableau de bord).
 """
+
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--create-db", help="create database",
+                        action="store_true", default=False)
+    
+    args = parser.parse_args()
+
+    if args.create_db:
+        print("--> Creating database")
+        
