f052564a273c88fa8bbec409fe6c842274e41297
lucasvn <lucasvn.pro@gmail.com>
2018-06-19 16:50:28 +0200

    [Update] download, create_dataframes, fill_database functions

--- irigo-app/etl.py
+++ irigo-app/etl.py
@@ -15,119 +15,123 @@ import datetime as dt
 # sql
 from sqlalchemy import create_engine
 
-# # Requêtage d'une API REST
+def download():
+    # Pour lancer une requête GET, c'est ultra-simple : on utilise `requests.get(url)`.
+    #
+    # Si vous voulez glisser des paramètres avec la requête, utilisez l'argument optionnel `params`, qui accepte un simple dictionnaire.
 
-# Pour lancer une requête GET, c'est ultra-simple : on utilise `requests.get(url)`.
-# 
-# Si vous voulez glisser des paramètres avec la requête, utilisez l'argument optionnel `params`, qui accepte un simple dictionnaire.
+    g = requests.get("https://data.angers.fr/api/records/1.0/search/",
+                     params={
+                         'dataset': 'bus-tram-position-tr',
+                         'rows': -1
+                     })
 
-g = requests.get("https://data.angers.fr/api/records/1.0/search/",
-                 params={
-                     'dataset': 'bus-tram-position-tr',
-                     'rows': -1
-                 })
+    # Pour tester que tout a bien fonctionnné, on va vérifié le statut.
+    g.raise_for_status()
+    # On peut construire une `DataFrame` directement à partir d'un "dictionnaires de colonnes".
+    dd = g.json()['records']
+    return dd
 
-# Regardons la nature de ce qui nous est renvoyé.
+def create_dataframes(d):
+    # Arret
+    id_arret = [elem['fields']['idarret'] for elem in d]
+    nom_arret = [elem['fields']['nomarret'] for elem in d]
+    mne_arret = [elem['fields']['mnemoarret'] for elem in d]
 
-# Pour tester que tout a bien fonctionnné, on va vérifié le statut.
+    arret = pd.DataFrame({
+        'id_arret': id_arret,
+        'nom_arret': nom_arret,
+        'mne_arret': mne_arret
+    })
 
-g.raise_for_status()
+    # Vehicule
+    id_vehicule = [elem['fields']['idvh'] for elem in d]
+    type_vehicule = [elem['fields']['type'] for elem in d]
+    etat_vehicule = [elem['fields']['etat'] for elem in d]
 
+    bus = pd.DataFrame({
+        'id_vehicule': id_vehicule,
+        'type_vehicule': type_vehicule,
+        'etat_vehicule': etat_vehicule
+    })
 
-# Si ça dit rien c'est que c'est bon :) Dans le doute, regardons le statut...
+    # Ligne
+    id_ligne = [elem['fields']['idligne'] for elem in d]
+    nom_ligne = [elem['fields']['nomligne'] for elem in d]
+    num_ligne = [elem['fields']['mnemoligne'] for elem in d]
 
-# # Remplissage de DataFrame
+    ligne = pd.DataFrame({
+        'id_ligne': id_arret,
+        'nom_ligne': nom_ligne,
+        'num_ligne': num_ligne
+    })
 
-# On peut construire une `DataFrame` directement à partir d'un "dictionnaires de colonnes".
+    # Trajet
+    id_vehicule = [elem['fields']['idvh'] for elem in d]
+    id_ligne = [elem['fields']['idligne'] for elem in d]
+    latitude = [elem['fields']['coordonnees'][0] for elem in d]
+    longitude = [elem['fields']['coordonnees'][1] for elem in d]
+    destination = [elem['fields']['dest'] for elem in d]
 
-dd = g.json()['records']
+    trajet = pd.DataFrame({
+        'id_vehicule': id_vehicule,
+        'id_ligne': id_ligne,
+        'latitude': latitude,
+        'longitude': longitude,
+        'destination': destination
+    })
 
-# Arret
-id_arret = [elem['fields']['idarret'] for elem in dd]
-nom_arret = [elem['fields']['nomarret'] for elem in dd]
-mne_arret = [elem['fields']['mnemoarret'] for elem in dd]
+    # Etape
+    id_arret = [elem['fields']['idarret'] for elem in d]
+    harret = [elem['fields']['harret'] for elem in d]
+    record_timestamp = [elem['record_timestamp'] for elem in d]
+    ecart = [elem['fields']['ecart'] for elem in d]
 
-arret = pd.DataFrame({
-    'id_arret': id_arret,
-    'nom_arret': nom_arret,
-    'mne_arret': mne_arret
-})
+    etape = pd.DataFrame({
+        'id_arret': id_arret,
+        'heure_arret_theorique': harret,
+        'ecart': ecart,
+        'record_timestamp': record_timestamp
+    })
 
-#Vehicule
-id_vehicule = [elem['fields']['idvh'] for elem in dd]
-type_vehicule = [elem['fields']['type'] for elem in dd]
-etat_vehicule = [elem['fields']['etat'] for elem in dd]
+    etape['record_timestamp'] = pd.to_datetime(etape['record_timestamp'])
 
-bus = pd.DataFrame({
-    'id_vehicule': id_vehicule,
-    'type_vehicule': type_vehicule,
-    'etat_vehicule': etat_vehicule
-})
+    def transfo(row):
+        return row['record_timestamp'] + dt.timedelta(seconds=row['ecart'])
 
-#Ligne
-id_ligne = [elem['fields']['idligne'] for elem in dd]
-nom_ligne = [elem['fields']['nomligne'] for elem in dd]
-num_ligne = [elem['fields']['mnemoligne'] for elem in dd]
+    etape['heure_arret_theorique'] = etape.apply(transfo, axis='columns')
 
-ligne = pd.DataFrame({
-    'id_ligne': id_arret,
-    'nom_ligne': nom_ligne,
-    'num_ligne': num_ligne
-})
+    d_df =  {
+        'arret': arret,
+        'bus': bus,
+        'ligne': ligne,
+        'trajet': trajet,
+        'etape': etape
+    }
 
-#Trajet
-id_vehicule = [elem['fields']['idvh'] for elem in dd]
-id_ligne = [elem['fields']['idligne'] for elem in dd]
-latitude = [elem['fields']['coordonnees'][0] for elem in dd]
-longitude = [elem['fields']['coordonnees'][1] for elem in dd]
-destination = [elem['fields']['dest'] for elem in dd]
+    return d_df
 
-trajet = pd.DataFrame({
-    'id_vehicule': id_vehicule,
-    'id_ligne': id_ligne,
-    'latitude': latitude,
-    'longitude': longitude,
-    'destination': destination
-})
+def fill_database(d_df):
+    # Ouverture de la connection vers la bdd
+    engine = create_engine("sqlite:///database.db")
+    connection = engine.connect()
 
-#Etape
-id_arret = [elem['fields']['idarret'] for elem in dd]
-harret = [elem['fields']['harret'] for elem in dd]
-record_timestamp = [elem['record_timestamp'] for elem in dd]
-ecart = [elem['fields']['ecart'] for elem in dd]
+    def add_index(df, tablename, indexname):
+        """Ajoute une nouvelle colonne  avec l'auto-increment"""
 
-etape = pd.DataFrame({
-    'id_arret': id_arret,
-    'heure_arret_theorique': harret,
-    'ecart': ecart,
-    'record_timestamp': record_timestamp
-})
+        # On va chercher dans la table le dernier increment
+        table =
 
-etape['record_timestamp'] = pd.to_datetime(etape['record_timestamp'])
+    # Table arret
+    arret.to_sql('arret', connection, if_exists='replace', index=False)
+    # Table bus
+    bus.to_sql('bus', connection, if_exists='replace', index=False)
+    # Table ligne
+    ligne.to_sql('ligne', connection, if_exists='replace', index=False)
+    # Table trajet
+    trajet.to_sql('trajet', connection, if_exists='replace', index=False)
+    # Table etape
+    etape.to_sql('etape', connection, if_exists='replace', index=False)
 
-def transfo(row):
-    return row['record_timestamp'] + dt.timedelta(seconds=row['ecart'])
-etape['heure_arret_theorique'] = etape.apply(transfo, axis='columns')
-
-
-# Ouverture de la connection vers la bdd
-engine = create_engine("sqlite:///database.db")
-connection = engine.connect()
-
-#Table arret
-arret.to_sql('arret', connection, if_exists='replace', index=False)
-
-#Table bus
-bus.to_sql('bus', connection, if_exists='replace', index=False)
-
-#Table ligne
-ligne.to_sql('ligne', connection, if_exists='replace', index=False)
-
-#Table trajet
-trajet.to_sql('trajet', connection, if_exists='replace', index=False)
-
-#Table etape
-etape.to_sql('etape', connection, if_exists='replace', index=False)
-
-#Fermeture connection
-connection.close()
+    # Fermeture connection
+    connection.close()
