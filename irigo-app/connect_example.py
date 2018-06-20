9f6b6dd6ceb1578a80ad8982e513d6682cef7d28
Pierre Girardeau <pierre.girardeau@coto-conseil.fr>
2018-06-20 10:15:50 +0200

    Added createe to connect_example.py

--- irigo-app/connect_example.py
+++ irigo-app/connect_example.py
@@ -1,5 +1,7 @@
 import json
 
+import pandas as pd
+
 from sqlalchemy import create_engine
 
 with open('db_settings.json', 'r') as file:
@@ -16,3 +18,15 @@ engine = create_engine('{dialect}+{driver}://{user}:{pwd}@{host}:{port}'
 
 print(engine.table_names())
 
+connection = engine.connect()
+
+df = pd.DataFrame({
+    'a': [1, 2, 3],
+    'b': [2, 3, 4]
+})
+
+df.to_sql('bidon', connection)
+
+connection.close()
+
+print(engine.table_names())
