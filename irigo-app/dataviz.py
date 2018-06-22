8b1fa6e81341d0c35f429835c7f15f1052904365
Dimitry Robin <dimy@hotmail.fr>
2018-06-22 10:42:48 +0200

    Add fonctionnal dropdown :)

--- irigo-app/dataviz.py
+++ irigo-app/dataviz.py
@@ -48,11 +48,10 @@ def get_dash():
                      .order_by(Etape.record_timestamp.desc()) \
                      .first()[0]
     results = session.query(Etape.ecart, Trajet.latitude, Trajet.longitude,
-                            Trajet.destination, Trajet.id_trajet) \
-                     .select_from(Etape).join(Trajet) \
+                            Trajet.destination, Trajet.id_trajet, Trajet.id_ligne, Ligne.nom_ligne) \
+                     .select_from(Etape).join(Trajet).join(Ligne) \
                      .filter(Etape.record_timestamp == lastUts)
-    print(lastUts)
-    print("{} points.".format(len(results.all())))
+
     colors = []
 
     for result in results:
@@ -70,12 +69,65 @@ def get_dash():
     # Contenu de l'app
     app.layout = html.Div([
         html.H1('Irigo app', style={'text-align': 'center'}),
+<<<<<<< c8a68bf2675eb675e49ba7be527a5d300a308e36
         html.Div(get_barh(lastUts)),
         html.Div(
             dcc.Graph(
                 id='map',
                 figure=get_map_figure(results, colors)
             )),
+=======
+        html.Div([
+            dcc.Dropdown(
+                id='select-ligne',
+                options=[{'label': trajet.nom_ligne, 'value': trajet.id_ligne} for trajet in results]
+            )
+        ]),
+        html.Div([
+            html.Div([
+                dcc.Graph(
+                    id='map',
+                    figure={
+                        'data': [
+                            go.Scattermapbox(
+                                lat=[trajet.latitude for trajet in results],
+                                lon=[trajet.longitude for trajet in results],
+                                mode='markers',
+                                marker=dict(
+                                    size=9,
+                                    color=colors
+                                ),
+                                text=[trajet.destination for trajet in results],
+                                customdata=[trajet.id_trajet for trajet in results]
+                            )
+                        ],
+                        'layout': go.Layout(
+                            height=700,
+                            autosize=True,
+                            hovermode='closest',
+                            mapbox=dict(
+                                accesstoken=mapbox_access_token,
+                                bearing=0,
+                                center=dict(
+                                    lat=np.mean([trajet.latitude for trajet in results]),
+                                    lon=np.mean([trajet.longitude for trajet in results])
+                                ),
+                                pitch=0,
+                                zoom=11
+                            ),
+                        )
+                    }
+                )
+            ], style={'width': '70%', 'float': 'left'}),
+            html.Div([
+                dcc.Markdown(d("""
+                    **Graphique à ajouter**
+
+                    Description du graphique
+                """))
+            ], style={'margin-top': '83px', 'float': 'right', 'width': '30%'})
+        ]),
+>>>>>>> Add fonctionnal dropdown :)
         html.Div([
             dcc.Markdown(d("""
                 **Données par point**
@@ -100,11 +152,22 @@ def get_dash():
         
         # Ouverture d'une session vers la DB
         session = Session()
+<<<<<<< c8a68bf2675eb675e49ba7be527a5d300a308e36
 
         query = session.query(Ligne.nom_ligne, Ligne.num_ligne,
                               Vehicule.type_vehicule, Vehicule.etat_vehicule) \
                        .select_from(Trajet).join(Ligne).join(Vehicule) \
                        .filter_by(id_trajet=clickData['points'][0]['customdata'])
+=======
+        if clickData:
+            query = session.query(Ligne.nom_ligne, Ligne.num_ligne, Vehicule.type_vehicule, Vehicule.etat_vehicule) \
+                       .select_from(Trajet).join(Ligne).join(Vehicule) \
+                       .filter(Trajet.id_trajet == clickData['points'][0]['customdata'])
+        else:
+            query = session.query(Ligne.nom_ligne, Ligne.num_ligne, Vehicule.type_vehicule, Vehicule.etat_vehicule) \
+                       .select_from(Trajet).join(Ligne).join(Vehicule)
+        session.close()
+>>>>>>> Add fonctionnal dropdown :)
 
         df = pd.read_sql_query(query.statement, query.session.bind)
 
@@ -112,7 +175,54 @@ def get_dash():
         
         return generate_table(df)
 
+<<<<<<< c8a68bf2675eb675e49ba7be527a5d300a308e36
     app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+=======
+    @app.callback(
+        dash.dependencies.Output('map', 'figure'),
+        [dash.dependencies.Input('select-ligne', 'value')])
+    def update_graph(value):
+        session = Session()
+        print(value)
+        if value == None:
+            lastUts = session.query(Etape.record_timestamp).order_by(Etape.id_etape.desc()).first()[0]
+            results = session.query(Etape.ecart, Trajet.latitude, Trajet.longitude, Trajet.destination, Trajet.id_trajet, Trajet.id_ligne, Ligne.nom_ligne).select_from(Etape).join(Trajet).join(Ligne).filter(Etape.record_timestamp == lastUts)
+        else:
+            lastUts = session.query(Etape.record_timestamp).order_by(Etape.id_etape.desc()).first()[0]
+            results = session.query(Etape.ecart, Trajet.latitude, Trajet.longitude, Trajet.destination, Trajet.id_trajet, Trajet.id_ligne, Ligne.nom_ligne).select_from(Etape).join(Trajet).join(Ligne).filter(Etape.record_timestamp == lastUts).filter(Trajet.id_ligne == value)
+        session.close()
+
+        return {
+            'data': [
+                go.Scattermapbox(
+                    lat=[trajet.latitude for trajet in results],
+                    lon=[trajet.longitude for trajet in results],
+                    mode='markers',
+                    marker=dict(
+                        size=9,
+                        color=colors
+                    ),
+                    text=[trajet.destination for trajet in results],
+                    customdata=[trajet.id_trajet for trajet in results]
+                )
+            ],
+            'layout': go.Layout(
+                height=700,
+                autosize=True,
+                hovermode='closest',
+                mapbox=dict(
+                    accesstoken=mapbox_access_token,
+                    bearing=0,
+                    center=dict(
+                        lat=np.mean([trajet.latitude for trajet in results]),
+                        lon=np.mean([trajet.longitude for trajet in results])
+                    ),
+                    pitch=0,
+                    zoom=11
+                ),
+            )
+        }
+>>>>>>> Add fonctionnal dropdown :)
 
     return app
 
